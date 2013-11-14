d6d775b08cd9685d69ddeff621576d78e0b91c2a
maricel86 <maricel.caciula@yahoo.com>
2013-11-14 18:48:15 +0200

    Testing Cvs library

--- GitProjectTest/Program.cs
+++ GitProjectTest/Program.cs
@@ -4,7 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using MathNet.Numerics.LinearAlgebra.Complex32;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearAlgebra.Generic;
+using MathNet.Numerics.LinearAlgebra.Double.IO;
+using System.IO;
+using MathNet.Numerics.LinearAlgebra.IO;
+using DataAccess;
+
 
 namespace GitProjectTest
 {
@@ -13,8 +19,28 @@ namespace GitProjectTest
         static void Main(string[] args)
         {
             {
+
+
+
+                DataTable dt = DataTable.New.ReadCsv(@"ata.csv");
+
+                var mat = new DenseMatrix(dt.Rows.Count(), dt.ColumnNames.Count());
+
+
+
+
+                foreach (var item in dt.Rows)
+                {
+                    //var row = item.Values.ToArray<double>();
+                }
+
+
+
+
+
+
                 var matrix = DenseMatrix.Identity(4);
-                Console.WriteLine(matrix.ToString());
+                Console.WriteLine(matrix);
                 Console.ReadKey();
             }
         }
