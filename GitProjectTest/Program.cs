35e000564fda15006c16f398e36ca7604510c8e6
Maricel86 <maricel.caciula@yahoo.com>
2013-11-23 13:34:55 +0200

    Refactored

--- GitProjectTest/Program.cs
+++ GitProjectTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows.Forms;
 using ILNumerics;
 
 
@@ -9,37 +10,39 @@ namespace GitProjectTest
 
         static void Main(string[] args)
         {
-            //var identityMatrix = ILMath.eye(5, 5);
-            //Console.WriteLine("Identity Matrix for 5 is :");
-            //Console.WriteLine(identityMatrix);
+            // ==================== Part 1: Basic Function ====================
+            Console.WriteLine("Running warmUpExercise ... ");
+            Console.WriteLine("5x5 Identity Matrix: ");
+            Console.WriteLine(WarmUpExercise());
+            Console.WriteLine("Program paused. Press any key to continue.");
+            Console.ReadKey();
 
 
+            //======================= Part 2: Plotting =======================
+            Console.WriteLine("Plotting Data ...");
             ILArray<double> matrix;
             using (var matRead = new ILMatFile(@"Data\data.mat"))
             {
                 matrix = matRead.GetArray<double>(0);
             }
 
-            //var form = new DisplayForm();
-            //form.SetData(matrix.Concat(ILMath.zeros(matrix.Size[0], 1), 1).T,
-            //             "Population of City in 10,000s",
-            //             "Profit in $10,000s");
+
+            Console.WriteLine("Program paused. close plot window to continue.");
+
+            PlotData(matrix[ILMath.full, 0], matrix[ILMath.full, 1]);
 
 
-            //Application.Run(form);
+
+
+            //=================== Part 3: Gradient descent ===================
+            Console.WriteLine("Running Gradient Descent ...");
 
             var m = matrix.Size[0];
             var x = ILMath.ones(m, 1).Concat(matrix[ILMath.full, 0], 1);
             var y = matrix[ILMath.full, 1];
             var theta = ILMath.zeros(2, 1);
 
-
-            var hypothesis = ILMath.multiply(x, theta);
-            var error = hypothesis - y;
-            var squareError = ILMath.pow(error, ILMath.array((double)2));
-            var cost = (1.0 / (2.0 * m)) * ILMath.sum(squareError);
-
-            Console.WriteLine(cost);
+            Console.WriteLine(ComputeCost(x, y, theta));
 
 
             Console.ReadKey();
@@ -47,5 +50,49 @@ namespace GitProjectTest
         }
 
 
+
+        public static ILArray<double> WarmUpExercise()
+        {
+            //============= YOUR CODE HERE ==============
+            // Instructions: Return the 5x5 identity matrix 
+
+            return ILMath.eye(5, 5);
+
+            // ===========================================
+        }
+
+
+        public static void PlotData(ILArray<double> x, ILArray<double> y)
+        {
+            // ====================== YOUR CODE HERE ======================
+            // Instructions: Plot the training to the screen.
+            //               Set the axes labels. Assume the 
+            //               population and revenue data have been passed in
+            //               as the x and y arguments of this function.
+
+            var count = x.Size[0];
+            var vector = x.Concat(y, 1).Concat(ILMath.zeros(count, 1), 1);
+
+            var form = new DisplayForm();
+            form.SetData(vector.T, "Population of City in 10,000s", "Profit in $10,000s");
+            Application.Run(form);
+
+            // ===========================================
+        }
+
+        public static ILArray<double> ComputeCost(ILArray<double> x, ILArray<double> y, ILArray<double> theta)
+        {
+            //===================== YOUR CODE HERE ======================
+            //Instructions: computes the cost using theta as the
+            //              parameter for linear regression to fit the data points in x and y
+
+            var count = x.Size[0];
+            var hypothesis = ILMath.multiply(x, theta);
+            var error = hypothesis - y;
+            var squareError = ILMath.pow(error, ILMath.array((double)2));
+            return (1.0 / (2.0 * count)) * ILMath.sum(squareError);
+
+            // ===========================================
+        }
     }
 }
