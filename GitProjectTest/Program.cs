96b387946ebc3833df80724f16d79757260c112b
Maricel86 <maricel.caciula@yahoo.com>
2013-11-23 11:00:05 +0200

    Working on cost function for ml ex1

--- GitProjectTest/Program.cs
+++ GitProjectTest/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Drawing;
-using System.Linq;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using ILNumerics;
-using ILNumerics.Drawing;
-using ILNumerics.Drawing.Plotting;
 
 
 namespace GitProjectTest
@@ -15,21 +10,47 @@ namespace GitProjectTest
 
         static void Main(string[] args)
         {
-            var identityMatrix = ILMath.eye(5, 5);
-            Console.WriteLine("Identity Matrix for 5 is :");
-            Console.WriteLine(identityMatrix);
-            
+            //var identityMatrix = ILMath.eye(5, 5);
+            //Console.WriteLine("Identity Matrix for 5 is :");
+            //Console.WriteLine(identityMatrix);
+
 
             ILArray<double> matrix;
             using (var matRead = new ILMatFile(@"Data\data.mat"))
             {
                 matrix = matRead.GetArray<double>(0);
             }
-            
-            var form = new DisplayForm();
-            form.SetData(matrix.Concat(ILMath.zeros(matrix.Size[0], 1), 1).T);
-            
-            Application.Run(form); 
+
+            //var form = new DisplayForm();
+            //form.SetData(matrix.Concat(ILMath.zeros(matrix.Size[0], 1), 1).T,
+            //             "Population of City in 10,000s",
+            //             "Profit in $10,000s");
+
+
+            //Application.Run(form);
+
+            var m = matrix.Size[0];
+            var x = ILMath.ones(m, 1).Concat(matrix[ILMath.full, 0], 1);
+            var y = matrix[ILMath.full, 1];
+            var theta = ILMath.zeros(2, 1);
+
+
+            var hypothesis = ILMath.multiply(x, theta);
+
+            var c = (hypothesis - y);
+
+
+
+            //Console.WriteLine(ILMath.pow(c, c));
+
+             
+
+
+            //Console.WriteLine(theta);
+
+            //Console.WriteLine(x*theta);
+
+
 
             Console.ReadKey();
 
